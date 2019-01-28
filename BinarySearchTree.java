8f89a41c589884a8e175c00044e6a14b8ece52d4
Amit Nautiyal <amitnautiyal.cs@gmail.com>
2019-01-28 16:23:16 +0530

    Update BinarySearchTree.java

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -80,6 +80,159 @@ public class BinarySearchTree{
         
     }
     
+    public boolean deleteNode(int key)
+    {
+        Node currentNode=root;
+        Node parentNode=root;
+        
+        boolean isLeftChild=false;
+        
+        //Searching to find the node with the key to delete
+        while(currentNode.key!=key)
+        {
+            parentNode=currentNode;
+            if(key<currentNode.key)
+            {
+                isLeftChild=true;
+                currentNode=currentNode.leftChild;
+            }
+            else {
+                currentNode= currentNode.rightChild;
+                isLeftChild=false;
+                
+            }
+            if(currentNode==null)
+            {
+                return false;
+                
+            }
+            
+        }
+        //At this point we found the Node
+        
+        Node nodetoDelete=currentNode;
+        
+        // if node is a leaf
+        if(nodetoDelete.leftChild==null && nodetoDelete.rightChild==null)
+        {
+            //
+            if(nodetoDelete==root)
+            {
+                root=null;
+            }
+            
+            else if(isLeftChild)
+            {
+                parentNode.leftChild=null;
+            }
+            else
+            {
+                
+                parentNode.rightChild=null;
+                
+            }
+        }
+        // if node has one Child that is on the left
+        
+        else if(nodetoDelete.rightChild==null)
+        {
+            
+            if(nodetoDelete==root)
+            {
+                
+                root=nodetoDelete.leftChild;
+            }
+            else if(isLeftChild)
+            {
+                parentNode.leftChild=nodetoDelete.leftChild;
+                
+            }
+            else{
+                
+                parentNode.rightChild= nodetoDelete.leftChild;
+            }
+        }
+        
+        
+        // if node has one Child that is on the right
+        else if(nodetoDelete.leftChild==null)
+        {
+            
+            if(nodetoDelete==root)
+            {
+                
+                root=nodetoDelete.rightChild;
+            }
+            else if(isLeftChild)
+            {
+                parentNode.leftChild=nodetoDelete.rightChild;
+                
+            }
+            else{
+                
+                parentNode.rightChild= nodetoDelete.rightChild;
+            }
+        }
+    // if node has two children(tricky)
+        
+    else
+    {
+        
+        Node successor=getSuccessor(nodetoDelete);
+        // here we find the minimum value and now we coonect parent of nodetoDelete tosuccessor instead
+        if(nodetoDelete==root)
+        {
+            root=successor;
+            
+        }
+        else if(isLeftChild)
+        {
+            
+            parentNode.leftChild=successor;
+            
+        }
+        else{
+            
+        parentNode.rightChild=successor;
+            
+        }
+        
+        successor.leftChild=nodetoDelete.leftChild;
+        
+        }
+        
+        
+        return true;
+        
+        
+    }
+    private Node getSuccessor(Node nodetoDelete)
+    {
+        Node successorParent= nodetoDelete;
+        Node successor=nodetoDelete;
+        
+        Node current=nodetoDelete.rightChild;//go to the right Child
+        while(current!=null)// start going left down the tree untill node has no left child
+        {
+            successorParent=successor;
+            successor=current;
+            current=current.leftChild;
+            
+        }
+        // if the successor is not a right child 
+        if(successor!=nodetoDelete.rightChild)
+        {
+            successorParent.leftChild=successor.rightChild;
+            successor.rightChild=nodetoDelete.rightChild;
+            
+        }
+        return successor;
+            
+        }
+        
+        
+    }
     
     
-}
+    
+    
