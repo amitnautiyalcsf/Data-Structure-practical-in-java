b69bf4d3f3ac05e32c7915845832118b9890775f
Amit Nautiyal <amitnautiyal.cs@gmail.com>
2019-01-29 17:06:09 +0530

    Create NodeClassForDoublyLinkedList.java

---
+++ NodeClassForDoublyLinkedList.java
@@ -0,0 +1,12 @@
+package ds.doublyLinkedList;
+public class Node{
+    public Node next=null;
+    public Node previous=null;
+    public int data;
+    public void displayNode()
+    {
+        
+    System.out.println("{"+data+"}");
+    }
+    
+}
